2a0adf54a2abfac57fa51f2fd381b6248ec250bd
Abdullah Acar <abdullahacar1107@gmail.com>
2020-07-10 17:40:29 +0300

    Add files via upload

--- proje/admin.py
+++ proje/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Yorum,Urun
+from .models import Yorum,Urun,Etiket
 
 admin.site.register(Yorum)
 admin.site.register(Urun)
-
-
+admin.site.register(Etiket)
